cd132a047a3b16b39f4ddeaf7315892e7869a372
evimansor <121963813+evimansor@users.noreply.github.com>
2023-09-21 07:29:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript code in script.js
+
+// Prompt the user for their name and store it in a variable
+var userName = prompt("What's your name?");
+
+// Create a message to greet the user
+var greeting = "Hello, " + userName + "! Welcome to our website.";
+
+// Display the greeting on the webpage
+document.write(greeting);
